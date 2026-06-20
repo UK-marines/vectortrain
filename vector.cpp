032dd280b5b6e3a79fe038481ca99e3814010bcb
KeyasH <kukuchipistolet@gmail.com>
2026-06-20 12:17:18 +0300

    more shit

--- vector.cpp
+++ vector.cpp
@@ -5,12 +5,17 @@ int main() {
     std::vector<int> damage_list = {100, 250, 500};
 
     std::cout << damage_list[0] << ", " << damage_list[1] << ", " << damage_list[2] << "\n";
-
-    for(int i = 0; i < 3; i++) {
-    	damage_list[i]+=50;
-    }
-    
-    std::cout << damage_list[0] << ", " << damage_list[1] << ", " << damage_list[2] << "\n";
+	for(int j = 0; j < 4; j++) {
+    	for(int i = 0; i < 3; i++) {
+    		damage_list[i]-=25;
+    		if(damage_list[i] == 0) {
+    			damage_list.erase(damage_list.begin() + i);
+    			i--; 
+    		} 
+			
+    	}
+	}    
+    std::cout << damage_list[0] << ", " << damage_list[1] << ", " << "\n";
 
     return 0;
 }
